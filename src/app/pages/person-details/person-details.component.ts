c93debb10b065bf4867eab33ec2a9021b66c08fd
Geovana Sembraski <geovana.nocera@gclaims.com.br>
2023-08-05 02:50:51 -0300

    Ajustes da agenda

--- src/app/pages/person-details/person-details.component.ts
+++ src/app/pages/person-details/person-details.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-person-datails',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./person-details.component.scss']
 })
 export class PersonDetailsComponent {
+  title = "Cadastro de pessoa";
 
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    var id = this.route.snapshot.paramMap.get('id');
+
+    if (!!id) this.title = "Editar Pessoa";
+  }
 }
